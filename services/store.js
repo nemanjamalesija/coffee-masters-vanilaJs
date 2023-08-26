16863df49b6c894dd154eefbb162ef9226d89860
nemanjamalesija <malesija13@gmail.com>
2023-08-26 14:41:46 +0200

    Add store proxy

--- services/store.js
+++ services/store.js
@@ -3,4 +3,19 @@ const store = {
   cart: [],
 };
 
-export default store;
+const proxiedStore = new Proxy(store, {
+  set(target, property, value) {
+    target[property] = value;
+    if (property == 'menu') {
+      window.dispatchEvent(new Event('appmenuchange'));
+    }
+
+    if (property == 'cart') {
+      window.dispatchEvent(new Event('appcartchange'));
+    }
+
+    return true;
+  },
+});
+
+export default proxiedStore;
